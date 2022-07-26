10aae8c3020d12f235a7f28eec164f92ad9b83c3
Jirka <86500701+jirkawq@users.noreply.github.com>
2022-07-26 20:07:23 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    printf("Oi, tudo bem? tenho 17 anos e programo. \n");
+    printf("Valor inteiro: %d \n", 10);
+    printf("Valor real: %f \n", 3.4324324);
+    printf("Valor real com apenas duas casas: %.2f\n ", 3.2348234312);
+    printf("Dado de texto: %c\n", 'a');
+    printf("Dado de texto: %s\n", "testando");
+    system("pause");
+    return 0;
+}

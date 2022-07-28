797a4f069acc58afd0183a8d6590cfc4875be883
Jirka <86500701+jirkawq@users.noreply.github.com>
2022-07-27 22:50:08 -0300

    Add files via upload

---
+++ strings4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+#define N 50
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    char s[N];
+    int tamanho_texto;
+
+    printf("Digite um texto: ");
+    gets(s);
+    tamanho_texto = strlen(s);
+    printf("Tamanho do texto: %d\n\n", tamanho_texto);
+
+    printf("Impressão de posição a posição:\n");
+    for (int i = 0; i < strlen(s); i++) {
+        printf("%c", s[i]);
+    }
+    return 0;
+}
